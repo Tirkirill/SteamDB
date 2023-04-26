8d3a49970e21e1278637e7c3def6b6641c95b7e8
tirkirill <sherbakirill@mail.ru>
2023-04-26 20:13:26 +0300

    Добавил процедуру очищения таблицы apps и последовательность действий

--- products.py
+++ products.py
@@ -3,8 +3,14 @@ import requests
 import psycopg2
 from settings import DB_CONFIGURATION_FILENAME, APP_LIST_FILENAME
 
+def get_db_params() -> dict:
+    with open(DB_CONFIGURATION_FILENAME, 'r') as f:
+        return json.load(f)
 
-def save_app_list():
+def save_app_list() -> None:
+    """
+    Записывает список приложений (список словарей с ключами appid и name) в файл
+    """
     res = requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json")
     if res.status_code == 200:
         data = res.json()["applist"]["apps"]
@@ -25,8 +31,7 @@ def load_app_list_sql() -> None:
     Вставляет данные (id и название) о приложениях в sql-таблицу apps
     """
 
-    with open(DB_CONFIGURATION_FILENAME, 'r') as f:
-        db_params = json.load(f)
+    db_params = get_db_params()
 
     with open(APP_LIST_FILENAME, 'r') as f:
         data = [(_dict["appid"], _dict["name"][:50]) for _dict in json.load(f)]
@@ -37,7 +42,30 @@ def load_app_list_sql() -> None:
     conn.commit()
     conn.close()
 
-load_app_list_sql()
+def clear_apps_table() -> None:
+    db_params = get_db_params()
+    conn = psycopg2.connect(**db_params)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM apps")
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    script_scenario = [
+        "save_app",
+        "load_app",
+        "clear_app"
+    ]
+
+    for action in script_scenario:
+        if action == "save_app":
+            save_app_list()
+        if action == "load_app":
+            load_app_list_sql()
+        if action == "clear_apps":
+            clear_apps_table()
+
+
 
 
 
