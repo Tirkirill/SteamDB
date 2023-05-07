4e19ce59ec1d82b7cc2d7eb5e4493205578356e2
tirkirill <sherbakirill@mail.ru>
2023-05-07 23:23:54 +0300

    Поправка

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 STEAM_GUARD_FILENAME = "guard.json"
 DB_CONFIGURATION_FILENAME = "dbconnect.json"
-APP_LIST_FILENAME = "AppList.json"
+APP_LIST_FILENAME = "Results/AppList.json"
 LOGGING_IS_REQUIRED = True
 LOG_FILENAME = "py_log.log"
